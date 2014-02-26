3e9ddcbdf6957131e49aa94fc03af16f3bc6510b
OtomeSound <s890081tonyhsu@gmail.com>
2014-02-27 06:26:12 +0800

    Sort the file to mvc type.

--- data/javascript/popup.js
+++ data/javascript/popup.js
@@ -4,10 +4,10 @@ function nowTime() {
 		var minutes = data.getMinutes();
 		var seconds = data.getSeconds();
 		var time ="";
-		time +=(hours > 12) ? "P.M. ": "A.M. ";
 		time += ((hours > 12) ? hours-12 : hours);
 		time += ((minutes <10) ? ":0" : ":") + minutes;
 		time += ((seconds <10) ? ":0" : ":") + seconds;
+		time +=(hours > 12) ? " P.M.": " A.M.";
 		$('#time').text(time);
 		setTimeout(nowTime,1000);
 }
