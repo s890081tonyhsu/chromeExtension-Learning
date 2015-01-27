277c7977114c35a1071203808c5306e93cfb0d50
OtomeSound <s890081tonyhsu@gmail.com>
2015-01-27 23:41:02 +0800

    Storage using example.

--- data/javascript/popup.js
+++ data/javascript/popup.js
@@ -11,4 +11,36 @@ function nowTime() {
 		$('#time').text(time);
 		setTimeout(nowTime,1000);
 }
+
+function lastMsg() {
+	chrome.storage.local.get('test', function(res){
+		$('#lastMsg span').text(res.test);
+	});
+	return;
+}
+
+function saveMsg() {
+	var data = $('#testMsg').val();
+	var saveAlert = cloneData(opt);
+	if(!data){
+		saveAlert.message = "Null Input";
+		chrome.notifications.create("saveReturn", saveAlert, function(notificationId){});
+		return;
+	}else{
+		chrome.storage.local.set({'test': data}, function(){
+			saveAlert.message = "Message Saved";
+			console.log(saveAlert);
+			chrome.notifications.create("saveReturn", saveAlert, function(notificationId){});
+		});
+		lastMsg();
+	}
+}
+
 $(document).ready(nowTime);
+$(document).ready(lastMsg);
+$(document).ready(function(){
+	$('#saveMsg').bind('click', saveMsg);
+	$('#connect').bind('click', function(){
+		callReturn("connect", '', connect);
+	});
+});
